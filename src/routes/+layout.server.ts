040c621e5f98302a422edb2aa19d5b74a2090600
agatemosu <agaatem@outlook.com>
2026-08-24 21:50:55 +0200

    add site name and fix img dimensions

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -11,12 +11,13 @@ export const load: LayoutServerLoad = async ({ url, locals }) => {
 		openGraph: {
 			type: 'website',
 			locale: 'es_ES',
+			siteName: '5 Digit Spanish Cup 2026',
 			images: [
 				{
 					url: new URL(header, url.origin).href,
 					alt: '5DSC2 Banner',
-					width: 800,
-					height: 600,
+					width: 1200,
+					height: 500,
 					type: 'image/png',
 				},
 			],
