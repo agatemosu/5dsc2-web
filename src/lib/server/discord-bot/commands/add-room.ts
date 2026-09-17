9dcc8cdae64d28db55bc2386511f1ddf021d25b9
Agatem <agaatem@outlook.com>
2026-09-17 18:07:33 +0200

    add player parameter to add-room

--- src/lib/server/discord-bot/commands/add-room.ts
+++ src/lib/server/discord-bot/commands/add-room.ts
@@ -1,6 +1,7 @@
 import { env } from '$env/dynamic/private';
 import { db } from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 import {
 	CommandContext,
 	CommandOptionType,
@@ -13,6 +14,7 @@ interface Options {
 	name: string;
 	date: string;
 	timezone: string | undefined;
+	player: string | undefined;
 }
 
 export class AddRoomCommand extends SlashCommand {
@@ -63,6 +65,17 @@ export class AddRoomCommand extends SlashCommand {
 						'es-ES': 'La zona horaria de la fecha, por defecto Europe/Madrid',
 					},
 				},
+				{
+					type: CommandOptionType.USER,
+					name: 'player',
+					name_localizations: {
+						'es-ES': 'jugador',
+					},
+					description: 'Add a player to the room',
+					description_localizations: {
+						'es-ES': 'Añadir un jugador a la sala',
+					},
+				},
 			],
 		});
 	}
@@ -86,18 +99,57 @@ export class AddRoomCommand extends SlashCommand {
 			`${options.date}[${options.timezone ?? 'Europe/Madrid'}]`,
 		);
 
-		const insertResult = await db
-			.insert(table.qualifierRooms)
-			.values({
-				id: options.name,
-				startTime: new Date(zdt.epochMilliseconds),
-			})
-			.onConflictDoNothing();
-
-		if (insertResult.rowsAffected === 0) {
-			return `Ya hay una sala con nombre ${options.name}.`;
+		let player: Pick<table.Player, 'id'> | undefined;
+		if (options.player !== undefined) {
+			player = await db.query.players.findFirst({
+				where: {
+					user: {
+						discordId: options.player,
+					},
+				},
+				columns: { id: true },
+			});
 		}
 
-		return `Nueva sala ${options.name} creada para <t:${zdt.epochMilliseconds / 1000}:F>.`;
+		if (options.player !== undefined && player === undefined) {
+			return 'Jugador no encontrado.';
+		}
+
+		const result = await db.transaction(async (tx) => {
+			const [insertResult] = await tx
+				.insert(table.qualifierRooms)
+				.values({
+					id: options.name,
+					startTime: new Date(zdt.epochMilliseconds),
+				})
+				.onConflictDoNothing()
+				.returning();
+
+			if (insertResult === undefined) {
+				return {
+					error: `Ya hay una sala con nombre ${options.name}.`,
+				};
+			}
+
+			if (player !== undefined) {
+				await tx
+					.update(table.players)
+					.set({
+						qualifierRoomId: insertResult.id,
+					})
+					.where(eq(table.players.id, player.id));
+			}
+
+			return { error: null };
+		});
+
+		if (result.error !== null) {
+			return result.error;
+		}
+
+		return (
+			`Nueva sala ${options.name} creada para <t:${zdt.epochMilliseconds / 1000}:F>.` +
+			(options.player === undefined ? '' : ` Añadido a <@${options.player}>.`)
+		);
 	}
 }
