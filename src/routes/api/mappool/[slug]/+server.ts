598074ab93e513939b4694b1d143d4f6a54e6f04
Agatem <agaatem@outlook.com>
2026-09-21 18:50:48 +0200

    add mappool api get

---
+++ src/routes/api/mappool/[slug]/+server.ts
@@ -0,0 +1,39 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { db } from '$lib/server/db';
+import { env } from '$env/dynamic/private';
+
+export const GET: RequestHandler = async (event) => {
+	if (import.meta.env.PROD) {
+		if (!event.locals.apiKey) {
+			return error(401, 'No API Key defined');
+		}
+
+		if (event.locals.apiKey !== env.API_KEY) {
+			return error(401, 'Invalid API Key');
+		}
+	}
+
+	const pool = await db.query.rounds.findFirst({
+		where: {
+			slug: event.params.slug,
+		},
+		with: {
+			mappools: {
+				with: {
+					beatmap: {
+						with: {
+							beatmapset: true,
+						},
+					},
+				},
+			},
+		},
+	});
+
+	if (!pool) {
+		error(404, 'Round not found');
+	}
+
+	return json(pool);
+};
