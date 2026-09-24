c591a8b1004f35e71c9778fba4721666ff745e5d
agatemosu <agaatem@outlook.com>
2026-09-24 10:54:41 +0200

    update leaderboard-stats.ts

--- src/lib/stats/leaderboard-stats.ts
+++ src/lib/stats/leaderboard-stats.ts
@@ -7,23 +7,25 @@ interface SoloStat {
 }
 
 export function groupMapScores(scores: ScoreWithPlayer[], maps: FullMappool[]): SoloStat[] {
-	const mapKeyed = new Map<string, FullMappool>();
-	for (const map of maps) {
-		const name = `${map.slotName}${map.slotIndex}`;
-		mapKeyed.set(name, map);
-	}
+	const mapKeyed = new Map(
+		maps.map((map) => [`${map.slotName}${map.slotIndex}`, map]),
+	);
 
 	const scoreGroups = Map.groupBy(scores, (score) => score.pick);
 
 	const result: SoloStat[] = [];
 	for (const [pick, scores] of scoreGroups) {
-		const highestScore = scores.reduce((max, current) =>
-			current.score > max.score ? current : max,
-		);
+		const sortedScores = scores.toSorted((a, b) => b.score - a.score);
+		const map = mapKeyed.get(pick);
+
+		if (!map || sortedScores.length === 0) {
+			continue;
+		}
+
 		result.push({
-			map: mapKeyed.get(pick) as FullMappool,
-			highestScore,
-			scores,
+			map,
+			highestScore: sortedScores[0],
+			scores: sortedScores,
 		});
 	}
 
