d9270c944a36c468aaa0ee6f79cad4e681f1162d
Agatem <agaatem@outlook.com>
2026-09-24 22:58:12 +0200

    fix sitemap

--- src/routes/players/+page.svelte
+++ src/routes/players/+page.svelte
@@ -15,19 +15,11 @@
 		{#if !data.allHaveSeed}
 			<PlayerGrid players={data.players} />
 		{:else}
-			<h2
-				class="-mt-2.5 mb-2.5 bg-dark py-2 text-center text-xl text-white"
-			>
-				clasificados
-			</h2>
+			<h2 class="-mt-2.5 mb-2.5 bg-dark py-2 text-center text-xl text-white">clasificados</h2>
 
 			<PlayerGrid players={data.players.slice(0, 32)} />
 
-			<h2
-				class="mt-2.5 mb-2.5 bg-dark py-2 text-center text-xl text-white"
-			>
-				no clasificados
-			</h2>
+			<h2 class="mt-2.5 mb-2.5 bg-dark py-2 text-center text-xl text-white">no clasificados</h2>
 
 			<PlayerGrid players={data.players.slice(32)} />
 		{/if}
--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,7 +1,9 @@
 import { StageType } from '$lib/enums';
 import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
 import * as sitemap from 'super-sitemap/sveltekit';
 import type { RequestHandler } from './$types';
+import { eq, exists } from 'drizzle-orm';
 
 export const GET: RequestHandler = async () => {
 	const rounds = await db.query.rounds.findMany({
@@ -9,6 +11,10 @@ export const GET: RequestHandler = async () => {
 			mappoolPublishedAt: { isNotNull: true },
 		},
 		columns: { slug: true, stageType: true },
+		extras: {
+			hasScores: (t) =>
+				exists(db.select().from(table.scores).where(eq(table.scores.roundId, t.id)).limit(1)),
+		},
 	});
 
 	return await sitemap.response({
@@ -18,6 +24,7 @@ export const GET: RequestHandler = async () => {
 			'/matches/[slug]': rounds
 				.filter((r) => r.stageType !== StageType.Qualifiers)
 				.map((r) => r.slug),
+			'/stats/[slug]': rounds.filter((r) => r.hasScores).map((r) => r.slug),
 		},
 	});
 };
