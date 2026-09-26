22b72f995080e9bfd93883ca32296c070631c834
agatemosu <agaatem@outlook.com>
2026-09-26 14:50:17 +0200

    add match endpoint

---
+++ src/routes/api/match/[id]/+server.ts
@@ -0,0 +1,16 @@
+import { db } from '$lib/server/db';
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async (event) => {
+	const match = await db.query.matches.findFirst({
+		where: { id: event.params.id },
+		columns: { teamRedPoints: true, teamBluePoints: true, rundown: true },
+	});
+
+	if (!match) {
+		return error(404, 'Match not found');
+	}
+
+	return json(match);
+};
